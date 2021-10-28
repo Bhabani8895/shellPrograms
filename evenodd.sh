f85647403500678962515236895be2c83582facc
Bhabani8895 <bhabaniprof2495@gmail.com>
2021-10-28 11:41:25 +0530

    Added changes

--- evenodd.sh
+++ evenodd.sh
@@ -4,8 +4,8 @@ read num
 r=$(($num%2))
 if [ $r -eq 0 ]
 then
-    echo "The number is even"
+    echo "The number is even bHABANI"
 else
-    echo "The number is odd"
+    echo "The number is odd BHABANI"
 fi
 
